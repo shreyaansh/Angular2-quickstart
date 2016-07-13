6a43d0f6a9cfb3916efd1dbdcbdd4de3c6bd6ed7
Shreyaansh Bassi <sbassi@purdue.edu>
2016-07-13 12:36:03 +0530

    Comment changes

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './courses.component', './authors.component', './publishers.components', './like.component', './main.component', './zippy.component'], function(exports_1, context_1) {
+System.register(['angular2/core', './zippy.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,27 +10,12 @@ System.register(['angular2/core', './courses.component', './authors.component', 
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1, authors_component_1, publishers_components_1, like_component_1, main_component_1, zippy_component_1;
+    var core_1, zippy_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (courses_component_1_1) {
-                courses_component_1 = courses_component_1_1;
-            },
-            function (authors_component_1_1) {
-                authors_component_1 = authors_component_1_1;
-            },
-            function (publishers_components_1_1) {
-                publishers_components_1 = publishers_components_1_1;
-            },
-            function (like_component_1_1) {
-                like_component_1 = like_component_1_1;
-            },
-            function (main_component_1_1) {
-                main_component_1 = main_component_1_1;
             },
             function (zippy_component_1_1) {
                 zippy_component_1 = zippy_component_1_1;
@@ -38,20 +23,20 @@ System.register(['angular2/core', './courses.component', './authors.component', 
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.post = {
+                        title: 'Title',
+                        isFavorite: true,
+                        isLiked: true
+                    };
                 }
-                /*post = {
-                    title: 'Title',
-                    isFavorite: true,
-                    isLiked: true
-                }*/
                 AppComponent.prototype.changeOccured = function () {
-                    //console.log("Change Occured, Event Updated!");
+                    console.log("Change Occured, Event Updated!");
                 };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
                         template: '<h1>My First Angular 2 App</h1><br><zippy>Hello</zippy><zippy>World</zippy>',
-                        directives: [courses_component_1.CoursesComponent, authors_component_1.AuthorsComponent, publishers_components_1.PublisherComponent, like_component_1.LikeComponent, main_component_1.MainComponent, zippy_component_1.ZippyComponent],
+                        directives: [zippy_component_1.ZippyComponent],
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,19 @@
 import {Component} from 'angular2/core';
-import {CoursesComponent} from './courses.component';
-import {AuthorsComponent} from './authors.component';
-import {PublisherComponent} from './publishers.components';
-import {LikeComponent} from './like.component';
-import {MainComponent} from './main.component';
 import {ZippyComponent} from './zippy.component';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1><br><zippy>Hello</zippy><zippy>World</zippy>',
-    directives: [CoursesComponent, AuthorsComponent, PublisherComponent, LikeComponent, MainComponent, ZippyComponent],
+    directives: [ZippyComponent],
 })
 export class AppComponent {
-    /*post = {
+    post = {
         title: 'Title',
         isFavorite: true,
         isLiked: true
-    }*/
+    }
 
     changeOccured() {
-        //console.log("Change Occured, Event Updated!");
+        console.log("Change Occured, Event Updated!");
     }
 }
