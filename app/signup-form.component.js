f1474d3fea39f4a378036967352cb5382e9517ee
Shreyaansh Bassi <sbassi@purdue.edu>
2016-07-20 12:36:52 +0530

    Added control objects

--- app/signup-form.component.js
+++ app/signup-form.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/common'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,17 +10,23 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, common_1;
     var SignUpFormComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
             }],
         execute: function() {
             SignUpFormComponent = (function () {
                 function SignUpFormComponent() {
-                    this.form = new ControlGroupObject;
+                    this.form = new common_1.ControlGroup({
+                        username: new common_1.Control(),
+                        password: new common_1.Control()
+                    });
                 }
                 SignUpFormComponent = __decorate([
                     core_1.Component({
--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -1,9 +1,13 @@
 import { Component } from 'angular2/core';
+import { ControlGroup, Control } from 'angular2/common';
 
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/signup-form.component.html'
 })
 export class SignUpFormComponent {
-    form = new ControlGroupObject;
+    form = new ControlGroup({
+        username: new Control(),
+        password: new Control()
+    });
 }
